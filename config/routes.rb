7e7c52cebb5b706281e2e43a417105f21df7074b
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-12-11 16:41:41 +0900

    add:dvise_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Rails.application.routes.draw do
+  #     devise_for :users, controllers: {
+  #       sessions: 'users/sessions'
+  #     }
+  #   end
+
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   root to: "pets#index"
   resources :pets do
     collection do
