c89a70ddd1e7a9ba96e2209e7426ba2dad55b591
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-12-15 17:52:29 +0900

    fixed:db_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,52 @@
+Group.destroy_all
+User.destroy_all
+Pet.destroy_all
+Wrap.destroy_all
 
-# User.destroy_all
-# 20.times do |n|
-#   user = User.create!(
-#     name: "seeduser#{n + 1}",
-#     email: "seed#{n + 1}@test.com",
-#     password: '123456',password_confirmation: '123456',
-#     admin: rand(2).zero?,
-#     icon:File.open("./app/assets/images/icon.png"),
-#     )
-#
-#     name = Faker::Creature::Dog.name
-#     birthday = Faker::Date.between(from: '2000/01/01', to: '2019/12/31')
-#     species = Faker::Creature::Dog.breed
-#     user.pets.create!(
-#       name: name,
-#       birthday: birthday,
-#       gender: rand(1..3),
-#       species: species,
-#       memo: "#{n + 1}#{name}は#{species}です",
-#       icon:File.open("./app/assets/images/icon.png"),
-#       )
-#
-# end
+20.times do |n|
+  icon = Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
+  user = User.create!(
+    name: "seeduser#{n + 1}",
+    email: "seed#{n + 1}@test.com",
+    password: '123456',password_confirmation: '123456',
+    admin: rand(2).zero?,
+    icon: icon,
+    )
+
+  2.times do |i|
+    group = user.groups.create!(
+      name: "#{user.name}group#{i + 1}",
+      owner_id: user.id,
+      icon:File.open("./app/assets/images/icon.png"),
+      )
+      3.times do |s|
+        icon = Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
+        group_user = User.create!(
+          name: "#{group.name}Assignuser#{s + 1}",
+          email: "#{group.name}Assign#{s + 1}@test.com",
+          password: '123456',password_confirmation: '123456',
+          admin: rand(2).zero?,
+          icon: icon,
+          )
+          group.members << group_user
+        end
+        5.times do |a|
+          name = Faker::Creature::Dog.name
+          birthday = Faker::Date.between(from: '2000/01/01', to: '2019/12/31')
+          species = Faker::Creature::Dog.breed
+          pet = group.pets.create!(
+            name: name,
+            birthday: birthday,
+            gender: rand(1..3),
+            species: species,
+            memo: "#{a + 1}#{name}は#{species}です",
+            icon:File.open("./app/assets/images/icon.png"),
+            )
+        end
+    end
+
+end
+
 
 # 20.times do |n|
 #   name = Faker::Games::Pokemon.name
