e8844a98323af894357ab658a00cc42d7faa0253
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-12-16 15:44:01 +0900

    fixed:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,29 +4,24 @@ Pet.destroy_all
 Wrap.destroy_all
 
 20.times do |n|
-  icon = Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
   user = User.create!(
     name: "seeduser#{n + 1}",
     email: "seed#{n + 1}@test.com",
     password: '123456',password_confirmation: '123456',
     admin: rand(2).zero?,
-    icon: icon,
     )
 
   2.times do |i|
     group = user.groups.create!(
       name: "#{user.name}group#{i + 1}",
       owner_id: user.id,
-      icon:File.open("./app/assets/images/icon.png"),
       )
       3.times do |s|
-        icon = Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
         group_user = User.create!(
           name: "#{group.name}Assignuser#{s + 1}",
           email: "#{group.name}Assign#{s + 1}@test.com",
           password: '123456',password_confirmation: '123456',
           admin: rand(2).zero?,
-          icon: icon,
           )
           group.members << group_user
         end
@@ -40,21 +35,8 @@ Wrap.destroy_all
             gender: rand(1..3),
             species: species,
             memo: "#{a + 1}#{name}は#{species}です",
-            icon:File.open("./app/assets/images/icon.png"),
             )
         end
     end
 
 end
-
-
-# 20.times do |n|
-#   name = Faker::Games::Pokemon.name
-#   email = Faker::Internet.email
-#   password = "123456"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                password_confirmation: password,
-#                )
-# end
