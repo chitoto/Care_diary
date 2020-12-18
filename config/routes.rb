d027ff905ce363044b148ba375523cd9baa9e8c1
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-12-19 01:16:34 +0900

    fixed:SNSlogin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: "groups#index"
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
     registrations: "users/registrations",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
