02a1383923277a10d69a136dd99c871127fd2b9d
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-01-28 22:56:13 +0900

    add_table:boards

---
+++ db/migrate/20210128134601_board.rb
@@ -0,0 +1,10 @@
+class Board < ActiveRecord::Migration[5.2]
+  def change
+    create_table :boards do |t|
+      t.string :title
+      t.text :content
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
