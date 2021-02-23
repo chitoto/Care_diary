d31e8c1643e67263a31e1acce8f71a0a772f7d3a
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-02-23 23:00:29 +0900

    add:relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@ class User < ApplicationRecord
   has_many :assigns, dependent: :destroy
   has_many :groups, through: :assigns
 
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.name = auth.info.name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_134601) do
+ActiveRecord::Schema.define(version: 2021_02_23_082622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,16 @@ ActiveRecord::Schema.define(version: 2021_01_28_134601) do
     t.datetime "updated_at", null: false
     t.bigint "group_id"
     t.index ["group_id"], name: "index_pets_on_group_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
